b268621b168c1f9f5f3e909083e8df5b9eab2190
Patrick Plaisted <patrick.plaisted@gmail.com>
2020-09-25 04:54:17 -0700

    Getting map to display

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiZ25vc2RlciIsImEiOiJja2ZkYWVlNWwwMTVwMnlteTMxaWRlN2JsIn0.udA0TQ6Nunk3REw8TpkEMw";
+const API_KEY = "pk.eyJ1IjoiZ25vc2RlciIsImEiOiJja2ZpNmFnM2wwOXRrMnhxZWJkN3ZpNXNiIn0.8pnvPXnUytI3GJEqTlRAvw";
