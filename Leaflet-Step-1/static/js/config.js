e505304428f2508a90f05872d45394cc23d15478
Patrick Plaisted <patrick.plaisted@gmail.com>
2020-09-25 04:20:29 -0700

    L1S1 complete

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoiZ25vc2RlciIsImEiOiJja2ZkYWVlNWwwMTVwMnlteTMxaWRlN2JsIn0.udA0TQ6Nunk3REw8TpkEMw";
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,2 @@
+// Step 1 -- get data set; all earthquakes past 7 days
+// https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
